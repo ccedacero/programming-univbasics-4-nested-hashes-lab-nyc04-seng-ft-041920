005ccc021f96507dd0dd598968fc84ad6b8916f6
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-22 15:06:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,12 +60,6 @@ programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-:yukihiro_matsumoto => {
-  :known_for => "Ruby",
-  :languages => ["LISP", "C"]
-}
-# return the entire updated hash
 
 	programmer_hash = 
  		{
@@ -82,10 +76,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto => {
+programmer_hash[
+:yukihiro_matsumoto => {
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
 }]
+
 programmer_hash
 end
 
