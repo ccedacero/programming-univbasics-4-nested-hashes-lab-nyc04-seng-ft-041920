32a7860dd52a2a1e5f897ffa3c03747ed1f83a48
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-04-22 15:23:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -99,7 +99,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-programmer_hash[:alan_kay][:known_for][0]="GUI"
+programmer_hash[:alan_kay] = 
+[:known_for]="GUI"
 # programmer_hash[:alan_kay][:known_for]
 end
 
